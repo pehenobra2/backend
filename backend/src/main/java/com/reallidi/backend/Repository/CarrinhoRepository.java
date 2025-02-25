26ff1f4001f0931765431cdf91331190f430ad8b
Pedro Henrique Nogueira Bragança <57445188+pehenobra2@users.noreply.github.com>
2025-02-25 11:44:52 -0300

    Create CarrinhoRepository.java

---
+++ backend/src/main/java/com/reallidi/backend/Repository/CarrinhoRepository.java
@@ -0,0 +1,10 @@
+package com.reallidi.backend.repository;
+
+import com.reallidi.backend.model.Carrinho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarrinhoRepository extends JpaRepository<Carrinho, Long> {
+
+}
