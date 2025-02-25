7ced31d66436814f2bfce61a97b197a23eaf4dd3
Pedro Henrique Nogueira Bragança <57445188+pehenobra2@users.noreply.github.com>
2025-02-25 11:13:04 -0300

    Create Adm.java

---
+++ backend/src/main/java/com/reallidi/backend/Model/Adm.java
@@ -0,0 +1,30 @@
+package com.reallidi.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "administrador")
+public class Administrador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dataCriacao;
+}
