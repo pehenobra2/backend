bf829ce002cee17de97704ed6c52addf93712cc5
Pedro Henrique Nogueira Bragança <57445188+pehenobra2@users.noreply.github.com>
2025-02-25 11:10:45 -0300

    Create Carrinho.java

---
+++ backend/src/main/java/com/reallidi/backend/Model/Carrinho.java
@@ -0,0 +1,36 @@
+package com.reallidi.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "carrinho")
+public class Carrinho {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_carrinho;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", nullable = false)
+    private Cliente cliente;
+
+    @Column(nullable = false)
+    private BigDecimal valorTotal = BigDecimal.ZERO;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dataCriacao;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime dataAtualizacao;
+
+}
